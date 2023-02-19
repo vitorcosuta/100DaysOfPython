5576bd7a2fa8de530b7b8b961e82eef423b4c0a1
Vitor Costa <73201828+vitorcosuta@users.noreply.github.com>
2023-02-19 20:17:51 -0300

    Added environment variables

--- Days-31-to-40/Day_35/rain-alert/main.py
+++ Days-31-to-40/Day_35/rain-alert/main.py
@@ -1,19 +1,18 @@
-# This code is not going to work because some confidential data has been omitted from the code.
-
+import os
 import requests
 from twilio.rest import Client
 
-OPW_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
-api_key = 'xxx'
-latitude = -19.917299
-longitude = -43.934559
-account_sid = 'xxx'
-auth_token = 'xxx'
+OWM_endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
+OWM_api_key = os.environ['OWM_API_KEY']
+latitude = -19.917299  # Belo Horizonte
+longitude = -43.934559  # Belo Horizonte
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 params = {
     'lat': latitude,
     'lon': longitude,
-    'appid': api_key,
+    'appid': OWM_api_key,
     'units': 'metric'
 }
 
@@ -21,7 +20,7 @@ params = {
 def forecast_rain():
     """Forecasts if it's going to rain in the next 12 hours"""
 
-    response = requests.get(OPW_ENDPOINT, params=params)
+    response = requests.get(OWM_endpoint, params=params)
     response.raise_for_status()
     weather_data = response.json()
     # Taking the next 12 hours (each index in the list refers to a 3-hour interval, so we need to slice the list down to
@@ -44,8 +43,8 @@ def send_SMS_rain_alert():
     message = client.messages \
         .create(
         body="It's going to rain today. Remember to bring an umbrella. ☔",
-        from_='+111',
-        to='Your phone number'
+        from_=os.environ['TWILIO_VIRTUAL_NUM'],
+        to=os.environ['MY_PHONE_NUM']
     )
 
     print(message.status)
